488bf4c250db2d9742fc461955d6f8d030561ba8
Camilo <ruiznho123@gmail.com>
2024-03-20 17:15:22 -0500

    Thumbnail generator

---
+++ lambda_function/main.py
@@ -0,0 +1,39 @@
+import boto3
+from PIL import Image
+
+s3 = boto3.client('s3')
+
+
+def image_resizer(filename, thumb_path):
+    """Download image and resize it to 100x100 pixels
+    Args:
+        filename: Image file name
+        thumb_path: Path to save the generated thumbnail"""
+
+    with Image.open(filename) as img:
+        img.thumbnail((100, 100))
+        img.save(thumb_path)
+
+
+def lambda_handler(event, context):
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = event['Records'][0]['s3']['object']['key']
+
+    try:
+        # Download image from S3
+        filename = key.split("/")[-1]
+        thumb_path = f'{filename.split(".")[0]}_thumb.jpg'
+
+        s3.download_file(bucket, key, filename)
+        image_resizer(filename, thumb_path)
+        s3.upload_file(thumb_path, bucket, f"resized/{thumb_path}")
+
+        return {
+            'statusCode': 200,
+            'body': 'Image resized and uploaded successfully'
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': f'Error: {str(e)}'
+        }
